fa064ef516a5f55bf86a319dcfe56ad0bf5b76a6
n720-hd <devandjayakarta@192.168.1.2>
2024-11-03 00:37:20 +0700

    feat: added login form and more auth features

--- apps/api/src/routers/index.ts
+++ apps/api/src/routers/index.ts
@@ -2,7 +2,6 @@ import express,{ Router } from "express";
 import authRouters from './auth.routers/index'
 
 const router = Router()
-
 router.use('*/images', express.static('src/public/images'))
 router.use('/api/auth', authRouters)
 
